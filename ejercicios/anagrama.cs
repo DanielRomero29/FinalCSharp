29fb238d52e90610058cac9d3e0100b630c3da8d
DanielRomero29 <danielromeroortega29@gmail.com>
2023-05-08 11:28:00 +0200

    optimizacion

--- ejercicios/anagrama.cs
+++ ejercicios/anagrama.cs
@@ -7,35 +7,37 @@ namespace anagrama
     {
         public static void anag()
         {
-            while(true){
+            while(true)
+            {
                 WriteLine("Introduce la primera palabra");
                 string? palabra1 = ReadLine();
                 WriteLine("Introduce la segunda palabra");
                 string? palabra2 = ReadLine();
-
-                if(palabra1 != "" || palabra2 != "")
+                
+                if(palabra1 != null && palabra2 != null)
                 {
-                    char[] letras1 = palabra1.ToLower().ToCharArray();
-                    char[] letras2 = palabra2.ToLower().ToCharArray();
-
-                    Array.Sort(letras1);
-                    Array.Sort(letras2);
-
-                    bool sonAnagramas = letras1.SequenceEqual(letras2);
-
-                    if (sonAnagramas)
+                    if(palabra1 != "" || palabra2 != "")
                     {
-                        WriteLine(palabra1 + " y " + palabra2 + " son anagramas.");
+                        char[] letras1 = palabra1.ToLower().ToCharArray();
+                        char[] letras2 = palabra2.ToLower().ToCharArray();
+
+                        Array.Sort(letras1);
+                        Array.Sort(letras2);
+
+                        bool sonAnagramas = letras1.SequenceEqual(letras2);
+
+                        if (sonAnagramas)
+                        {
+                            WriteLine(palabra1 + " y " + palabra2 + " son anagramas.");
+                        }
+                        else
+                        {
+                            WriteLine(palabra1 + " y " + palabra2 + " no son anagramas.");
+                        }
+                        break;
                     }
-                    else
-                    {
-                        WriteLine(palabra1 + " y " + palabra2 + " no son anagramas.");
-                    }
-                    break;
                 }
-                else WriteLine("Introduce palabras correctas");
             }
-            
         }
     }
 }
